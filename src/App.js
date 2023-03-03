903f942b5576e5d00e333edc94f03a3faa758570
Luis A <almonteluis92@gmail.com>
2023-03-03 08:28:27 -0500

    final touches to the code. Im done for now but im coming back to style this app better maybe with tailwindcss or something.

--- src/App.js
+++ src/App.js
@@ -22,6 +22,12 @@ function App() {
     return data;
   };
 
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
   // Add Task
   const addTask = async (task) => {
     const res = await fetch("http://localhost:5000/tasks", {
@@ -45,8 +51,19 @@ function App() {
   };
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
-    console.log(id);
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
         task.id === id ? { ...task, reminder: !task.reminder } : task
